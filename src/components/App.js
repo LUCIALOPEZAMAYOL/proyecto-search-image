f32d979f3d7f5e547e8d9f2156f15f4ad4390e44
Lucia <llopeza@alumno.unsm.edu.pe>
2022-04-28 11:26:17 -0500

    Petición Api pixabay con axios e importación de estilos

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import SearchInput from './SearchInput';
+import axios from 'axios';
+import ImageList from './ImageList';
+import '../style/App.css'
 
-const App = () => {
-    return(
-        <div className='ui container' style={{marginTop: '30px'}}>
-            <SearchInput/>
-        </div>
-    )
+class App extends React.Component {
+
+    state = { images: [] }
+
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get(`https://pixabay.com/api/?key=27057638-84768c1340507073def339350&q=${entry}&image_type=photo`);
+        this.setState({ images:response.data.hits })
+    }
+
+    render() {
+        return(
+            <div className='container'>
+                <div className='boxSearchInput'>
+                    <SearchInput onSearchSubmit = {this.onSearchSubmit} />
+                </div>
+                <div className='boxImageList'>
+                    <ImageList images={this.state.images} />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
